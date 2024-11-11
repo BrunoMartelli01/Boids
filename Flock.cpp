af2cd7700c3aa2ee8bb4b6a766f4f32c446c43c8
Bruno <buno.martelli2001@gmail.com>
2024-11-11 17:07:32 +0100

    bugfix same seed

--- Flock.cpp
+++ Flock.cpp
@@ -90,7 +90,7 @@ void Flock::createBoids()
 
 	void Flock::update()
 	{
-#pragma omp parallel for
+//#pragma omp parallel for
 		// Update boids
 		for (auto& boid : all_boids)
 		{
--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 int main() {
     srand(10);
     sq::Flock flock(0.8);
-    //flock.run();
+    flock.run();
 
     double time_sequential = flock.test(1000);
+    srand(10);
     parallel::Flock flock_parallel(0.8);
-    //flock_parallel.run();
+    flock_parallel.run();
     double time_parallel = flock_parallel.test(1000);
     double speedup = time_sequential / time_parallel;
     std::cout <<"speedup: "<< speedup << std::endl;
